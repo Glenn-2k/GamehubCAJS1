b0adcc94639a80b9060d062dd3bb569d904bc906
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2023-11-01 10:55:29 +0100

    Product specific
    started js for product specifc

--- JS/products/game.js
+++ JS/products/game.js
@@ -1,3 +1,5 @@
-export async function gamePage() {
-    
-}
+import { apiUrl } from "../api.js";
+
+export async function gamePage() {}
+
+const productSpecific = document.querySelector(".product-specific-content");
--- JS/products/games.js
+++ JS/products/games.js
@@ -2,39 +2,35 @@ import { apiUrl } from "../api.js";
 
 const gameTitle = document.querySelector(".games-container");
 
-
-
 export async function getGames() {
-    const response = await fetch(apiUrl);
-    const results = await response.json();
-    // console.log(results);
-    return results;
+  const response = await fetch(apiUrl);
+  const results = await response.json();
+  // console.log(results);
+  return results;
 }
 
 const game = await getGames();
-console.log(game);
+// console.log(game);
 
 const gameArray = Array.from(game);
 
-
 export async function gamesPage() {
-    const games = await getGames
-    displayGames(games);
-    // console.log(games);
+  const games = await getGames;
+  displayGames(games);
+  // console.log(games);
 }
 
-
 export function displayGames(gamesList) {
-    gameArray.forEach((game) => {
-        console.log(game.title);
-        console.log(game.description);
-        gameTitle.innerHTML += `
+  gameArray.forEach((game) => {
+    // console.log(game.title);
+    // console.log(game.description);
+    gameTitle.innerHTML += `
         <div class="game">
         <a href="/product-specific.html?id=${game.id}"><img src="${game.image}"
                                     alt="image of ${game.title} cover" class="games-cover"></a>
         <h2 class="games-title">${game.title}</h2>
         <p class="price">${game.price}</p>
         <a href="/cart-full.html" id="add-to-cart">Add to cart</a>
-        </div>`
-    });
-} 
+        </div>`;
+  });
+}
