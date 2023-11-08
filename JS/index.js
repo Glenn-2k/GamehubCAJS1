a08ac652b44957c0e508659c5e3263caf1da8a1e
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2023-11-08 11:47:02 +0100

    new path

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,9 @@
 import { apiUrl } from "./api.js";
-import { gamesPage } from "/JS/products/games.js";
-import { getGames } from "/JS/products/games.js";
+import { gamesPage } from "../JS/products/games.js";
+import { getGames } from "../JS/products/games.js";
 import { gamePage } from "./products/game.js";
 
-if (location.pathname === "/games.html") {
+if (location.pathname.endsWith("games.html")) {
   gamesPage();
 }
 
